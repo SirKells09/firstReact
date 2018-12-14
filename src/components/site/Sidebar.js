f4d620734927b0947dd67fbe53015f481e611f98
Kelly Williams <Kewilliams591@gmail.com>
2018-12-14 12:49:39 -0500

    fixed silly issues

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -21,9 +21,9 @@ const Sidebar = () => (
 
     <div className= "sidebar">
         <div className="sidebar-list-styling">
-            <ul className="sidebar-list -list unstyled">
+            <ul className="sidebar-list list-unstyled">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/rationale">Rationale</Link></li>
+                <li><Link to="/resources">Resources</Link></li>
                 <li><Link to="/functionalcomponent">Functional Component</Link></li>
                 <li><Link to='/jsxrules'>JSX Rules</Link></li>
                 <li><Link to ="/classcomponentdemo">Class Components</Link></li>
@@ -39,7 +39,7 @@ const Sidebar = () => (
         </div>
          <div className="sidebar-route">
             <Switch>
-                <Route exact path="/home"><Home /></Route>
+                <Route exact path="/"><Home /></Route>
                 <Route exact path="/resources"><Resources /></Route>
                 <Route exact path ="/functionalcomponent"><FunctionalComponentDemo /></Route>
                 <Route exact path="/jsxrules"><JSXRules /></Route>
