0893140affc1a72f1374072e235504a328e8cb99
Kelly Williams <Kewilliams591@gmail.com>
2018-12-14 12:58:51 -0500

    updated and looking good

--- src/components/concepts/LifeCycleCodepen.js
+++ src/components/concepts/LifeCycleCodepen.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const codeDiv = styled.div `
+const codeDiv = styled.div`
 
 `
 
 class LifeCycleDiagram extends Component {
-  render () {
+  render() {
 
     return (
       <div className='main'>
         <div className='mainDiv'>
           <h1>Life Cycle Codepen</h1>
-          <iframe className='col-md-12' title="Codepen Project" height="350vh" width="100%" src="https://codepen.io/ccharris/pen/wxdKYa">
+          <iframe className='col-md-12' title="Codepen Project" height="750vh" width="100%" src="https://codepen.io/ccharris/pen/wxdKYa">
           </iframe>
         </div>
       </div>
